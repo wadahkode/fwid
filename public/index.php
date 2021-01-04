82e5f64d93f55fe1512a5fdd09dc55b5a8fa8a3a
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-04 09:15:53 +0700

    up

--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
-
+/**
+ * Bootstraped
+ * 
+ * @return object $app
+ */
 $app = require_once __DIR__.'/../lib/bootstrap/app.php';
 
+/**
+ * Mencocokkan dengan versi yang terinstall dimesin.
+ * 
+ * @available php_version
+ * @available extension_loaded
+ * @custom []
+ */
 $app->compatible(array(
   'php_version' => (bool) version_compare(PHP_VERSION, '7.0.0', '<'),
   'extension_loaded' => array(
