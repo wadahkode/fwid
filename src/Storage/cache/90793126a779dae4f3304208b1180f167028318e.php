201bc9e58884c996437edd1d3d66db6a27f2203c
wadahkode <mvp.dedefilaras@gmail.com>
2021-01-05 10:59:54 +0700

    use blade engine

--- src/Storage/cache/90793126a779dae4f3304208b1180f167028318e.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('title', 'Administrator | Wadahkode'); ?>
-
-<?php $__env->startSection('content'); ?>
-  <h1>Hello admin</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /storage/emulated/0/htdocs/php/framework/src/Templates/admin/index.blade.php ENDPATH**/ ?>
