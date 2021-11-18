0da6d7cb6eb12ab02e1329e73faae5d6ad3ae548
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-18 23:46:24 +0700

    set tailwindcss for default stylesheet

--- src/Templates/layouts/app.blade.php
+++ src/Templates/layouts/app.blade.php
@@ -6,11 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
   <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+  <link rel="stylesheet" href="css/wadahkode.css">
 </head>
 <body>
   <div id="root">
-    <header class="fixed-top">@yield('header')</header>
-    <div class="cover container bg-dark pt-5 pb-4">@yield('cover')</div>
+    <header class="fixed top-0 w-full">@yield('header')</header>
+    <div class="h-screen">@yield('cover')</div>
     <main class="container mt-3">@yield('main')</main>
   </div>
 </body>
--- src/Templates/welcome.blade.php
+++ src/Templates/welcome.blade.php
@@ -3,17 +3,19 @@
 @section('title', $title)
 
 @section('header')
-  <nav>
-    <div>
-      <h1>Wadahkode</h1>
+  <nav class="py-6">
+    <div class="container mx-auto">
+      <h1 class="text-2xl font-semibold text-white">Wadahkode</h1>
     </div>
   </nav>
 @endsection
 
 @section('cover')
-  <div>
-    <h1>Selamat datang di wadahkode</h1>
-    <p>Sarana untuk saling berbagi ilmu mengenai teknologi dan sebagai wadah untuk saling berdiskusi.</p>
-    <a href="about">Lihat selengkapnya</a>
+  <div class="cover flex items-center h-screen">
+    <div class="container mx-auto">
+      <h1 class="text-4xl font-semibold text-white">Selamat datang di wadahkode</h1>
+      <p class="text-xl my-10 text-white break-words w-1/2 min-w-min">Sarana untuk saling berbagi ilmu mengenai teknologi dan sebagai wadah untuk saling berdiskusi.</p>
+      <a href="about" class="bg-blue-600 px-3 py-2 rounded-md text-white font-semibold">Lihat selengkapnya</a>
+    </div>
   </div>
 @endsection
