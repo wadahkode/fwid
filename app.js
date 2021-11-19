44e5adb11f3ddbb25f533473b6a4d6ea79b43546
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-20 06:58:13 +0700

    use service-worker on production

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("js/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.error(
+          "Service Worker registration failed: ",
+          registrationError
+        );
+      });
+  });
+}
