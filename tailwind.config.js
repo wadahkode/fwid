de00231fc47ab4448d665f6c3a535d7c2e87575b
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-21 21:28:07 +0700

    auto purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./app.js", "./styles.css"],
-  },
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
