7fdeb769867b2316f56ed20facc4befc0725ca71
wadahkode <mvp.dedefilaras@gmail.com>
2021-11-22 16:35:09 +0700

    forget uncomment in gitignore for session class

---
+++ lib/Wadahkode/Http/Session/SessionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wadahkode\Http\Session;
+
+class SessionFactory
+{
+  public function __construct()
+  {
+    session_start();
+  }
+
+  static public function getInstance()
+  {
+    return new Self();
+  }
+
+  public function __call($name, $arguments)
+  {
+    return call_user_func_array([$this, $name], $arguments);
+  }
+
+  protected function get($name)
+  {
+    return isset($_SESSION[$name]) ? $_SESSION[$name] : null;
+  }
+
+  protected function set($name, $value)
+  {
+    return $_SESSION[$name] = $value;
+  }
+}
