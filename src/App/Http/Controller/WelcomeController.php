e9a0e9a08d756dd1db97b3e12aa97baa0a1659c6
wadahkode <mvp.dedefilaras@gmail.com>
2021-12-03 18:13:13 +0700

    update

--- src/App/Http/Controller/WelcomeController.php
+++ src/App/Http/Controller/WelcomeController.php
@@ -4,16 +4,10 @@ namespace App\Http\Controller;
 
 class WelcomeController extends Controller
 {
-  public function __construct()
-  {
-    $this->pages = $this->model('pages')->findAll();
-  }
-
   public function index()
   {
     return view("welcome", [
-      "title" => "Welcome to wadahkode",
-      "pages" => $this->pages
+      "title" => "Welcome to wadahkode"
     ]);
   }
 }
