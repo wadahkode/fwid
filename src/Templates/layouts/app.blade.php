15252930e56cf2b8198d89794b687513db2e2be3
wadahkode <mvp.dedefilaras@gmail.com>
2021-12-04 19:52:37 +0700

    update

--- src/Templates/layouts/app.blade.php
+++ src/Templates/layouts/app.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
-  <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+  <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/x-icon" />
   @if (file_exists('dist/styles.css'))
-    <link rel="stylesheet" href="dist/styles.css">
+    <link rel="stylesheet" href="{{asset('dist/styles.css')}}">
   @endif
 </head>
 <body class="antialised bg-gray-200">
